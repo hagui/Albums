f01afb0e53e754a5eb73ca154b2b576c8c3eaf58
hagui <hagguizied7@gmail.com>
2019-04-27 22:17:48 +0200

    Initial commit

---
+++ app/src/main/java/com/tuto/leboncointest/model/Song.kt
@@ -0,0 +1,9 @@
+package com.tuto.leboncointest.model
+
+data class Song(
+    val albumId: Int,
+    val id: Int,
+    val thumbnailUrl: String,
+    val title: String,
+    val url: String
+)
---
+++ app/src/test/java/com/tuto/leboncointest/model/SongTest.kt
@@ -0,0 +1,36 @@
+package com.tuto.leboncointest.model
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class SongTest {
+
+    @Test
+    fun getAlbumId() {
+    }
+
+    @Test
+    fun getId() {
+    }
+
+    @Test
+    fun getThumbnailUrl() {
+    }
+
+    @Test
+    fun getTitle() {
+    }
+
+    @Test
+    fun getUrl() {
+    }
+
+    @Test
+    fun copy() {
+    }
+
+    @Test
+    fun equals() {
+    }
+}
